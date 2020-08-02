47491f9e1ef74febbfaa7c7d607f03d5797e3dd5
Max Xie <maxhxie@gmail.com>
2020-08-02 14:54:55 +0200

    add csrf token to form

--- english/views.py
+++ english/views.py
@@ -4,8 +4,8 @@ from .forms import UploadFileForm
 
 # Create your views here.
 class IndexView(generic.edit.FormView):
-    form_class = UploadFieldForm
-    template_name = 'upload.html'  # Replace with your template.
+    form_class = UploadFileForm
+    template_name = 'english/index.html'  # Replace with your template.
     success_url = '...'  # Replace with your URL or reverse().
 
     def post(self, request, *args, **kwargs):
@@ -14,7 +14,7 @@ class IndexView(generic.edit.FormView):
         files = request.FILES.getlist('file_field')
         if form.is_valid():
             for f in files:
-                pass  # Do something with each file.
+                print(f.title)
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
