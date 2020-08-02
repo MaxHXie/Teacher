59a909189d621f19df6301cf50168bef9d69fd9c
Max Xie <maxhxie@gmail.com>
2020-08-02 14:50:17 +0200

    fill in models.py

--- english/models.py
+++ english/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Essay(models.Model):
+    title = models.CharField(max_length=200)
+    file = models.FileField()
+    upload_datetime = models.DateTimeField('date uploaded')
+    paid = models.BooleanField()
+
+    def __str__(self):
+        return self.title
--- english/views.py
+++ english/views.py
@@ -1,9 +1,20 @@
 from django.views import generic
+from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
-class IndexView(generic.ListView):
-    template_name = 'english/index.html'
-    context_object_name = 'latest_question_list'
+class IndexView(generic.edit.FormView):
+    form_class = UploadFieldForm
+    template_name = 'upload.html'  # Replace with your template.
+    success_url = '...'  # Replace with your URL or reverse().
 
-    def get_queryset(self):
-        pass
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        files = request.FILES.getlist('file_field')
+        if form.is_valid():
+            for f in files:
+                pass  # Do something with each file.
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
