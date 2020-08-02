db6dafd2dd64231a2c3b2602ecdf2fd9c3acba91
Max Xie <maxhxie@gmail.com>
2020-08-02 17:49:18 +0200

    add essay app first model to admin

--- english/admin.py
+++ english/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Essay
+
+class EssayAdmin(admin.ModelAdmin):
+    fields = [
+    'title',
+    'file',
+    'upload_datetime',
+    'words',
+    'price',
+    'paid',
+    ]
+    list_display = ('title', 'file', 'upload_datetime', 'words', 'price', 'paid')
+
+admin.site.register(Essay, EssayAdmin)
--- english/models.py
+++ english/models.py
@@ -4,8 +4,8 @@ class Essay(models.Model):
     title = models.CharField(max_length=200)
     file = models.FileField()
     upload_datetime = models.DateTimeField('date uploaded')
-    words = models.IntegerField()
-    price = models.IntegerField()
+    words = models.IntegerField(null=True)
+    price = models.IntegerField(null=True)
     paid = models.BooleanField()
 
     def __str__(self):
