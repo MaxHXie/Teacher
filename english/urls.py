77b362f0a0741d9bcbf1b71bd3bff21400869336
Max Xie <maxhxie@gmail.com>
2020-08-02 17:45:09 +0200

    fix file upload form

--- english/urls.py
+++ english/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 app_name = 'english'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- english/views.py
+++ english/views.py
@@ -1,20 +1,16 @@
-from django.views import generic
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import UploadFileForm
 
-# Create your views here.
-class IndexView(generic.edit.FormView):
-    form_class = UploadFileForm
-    template_name = 'english/index.html'  # Replace with your template.
-    success_url = '...'  # Replace with your URL or reverse().
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('file_field')
+def index(request):
+    if request.method == 'POST':
+        print("POST")
+        form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            for f in files:
-                print("received file")
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+            print("YAS")
+            # file is saved
+            form.save()
+            return HttpResponseRedirect(reverse('english:index'))
+    else:
+        form = UploadFileForm()
+    return render(request, 'english/index.html', {'form': form})
