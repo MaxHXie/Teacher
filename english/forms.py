07e45ba39ac64b49b199170bdd3b3dfc65864aa1
Max Xie <maxhxie@gmail.com>
2020-08-07 13:11:43 +0200

    add front end for index page

--- english/forms.py
+++ english/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Essay
 
-class EssayForm(ModelForm):
+class EssayForm(forms.ModelForm):
     class Meta:
         model = Essay
         fields = ['email', 'essay_text']
+
+        widgets = {
+            'email': forms.TextInput(attrs={'class':'form-control transparent', 'placeholder':'Your email here'}),
+            'essay_text': forms.Textarea(attrs={'class':'form-control transparent', 'placeholder':'Copy paste your essay here', 'rows':14}),
+        }
