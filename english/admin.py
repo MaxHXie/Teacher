4ef5a8bdf6330e7ad2d9f210bc3f5c555045796a
Max Xie <maxhxie@gmail.com>
2020-08-23 08:49:59 +0200

    add default sort on upload_datetime in django admin

--- english/admin.py
+++ english/admin.py
@@ -18,4 +18,6 @@ class EssayAdmin(admin.ModelAdmin):
     list_display = ('essay_id', 'email', 'upload_datetime', 'characters', 'price', 'currency', 'paid', 'completed', 'were_limited', 'shorter_essay_text')
     readonly_fields = ['essay_id']
 
+    ordering = ('-upload_datetime',)
+
 admin.site.register(Essay, EssayAdmin)
