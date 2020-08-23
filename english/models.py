e7c01c2510c31afbea9e351d85a743fc52421e38
Max Xie <maxhxie@gmail.com>
2020-08-23 08:40:29 +0200

    change the way timestamp is defined in model so that we push the exact timestamp of when the essay was submitted to the website

--- english/models.py
+++ english/models.py
@@ -7,7 +7,7 @@ class Essay(models.Model):
     essay_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     essay_text = models.TextField(max_length=200000) #About 1 MB in Unicode, #80 pages of text
     email = models.EmailField(null=True)
-    upload_datetime = models.DateTimeField('date uploaded', default=timezone.now())
+    upload_datetime = models.DateTimeField('date uploaded', auto_now_add=True)
     characters = models.IntegerField(null=True)
     price = models.FloatField(null=True)
     currency = models.CharField(max_length=32, default="EUR")
