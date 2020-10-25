2bde4d541ac6e5b65255e2e14888d5fa3debab53
Max Xie <maxhxie@gmail.com>
2020-10-25 20:51:20 +0100

    update models

--- english/models.py
+++ english/models.py
@@ -6,7 +6,7 @@ import uuid
 class Essay(models.Model):
     essay_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     essay_text = models.TextField(max_length=200000) #About 1 MB in Unicode, #80 pages of text
-    email = models.EmailField(null=True)
+    email = models.EmailField(null=True, blank=True)
     upload_datetime = models.DateTimeField('date uploaded', auto_now_add=True)
     characters = models.IntegerField(null=True)
     price = models.FloatField(null=True)
@@ -15,7 +15,6 @@ class Essay(models.Model):
     completed = models.BooleanField(default=False)
     essay_correction_json = models.TextField(default= "", max_length=500000)
     errors = models.TextField(default= "", max_length=100000)
-    were_limited = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.essay_id)
