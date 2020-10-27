9acfbbf2514e21445c77c432a5c526cf15ef39cc
Max Xie <maxhxie@gmail.com>
2020-10-27 22:41:45 +0100

    add azure grammar check and change struture of how essays are corrected and stored

--- english/views.py
+++ english/views.py
@@ -26,7 +26,89 @@ def index(request):
     return render(request, 'english/index.html', {'form': form})
 
 def submit(request, essay):
+
     # submit as many APIs as you want
+    # format api responses to be in this format
+    """
+    {
+        "errors" :
+        [
+            {
+                "offset": 353,
+                "length": 12,
+                "message": "This word is a possibel misspelling",
+                "type": "Misspelling"
+                "error": "flexibly",
+                "correction": [
+                    "flexible",
+                    "flexibility",
+                    "flexibale"
+                ]
+            }
+        ],
+        [
+            {
+                "offset": 353,
+                "length": 12,
+                "message": "This word is a possibel misspelling",
+                "type": "Misspelling"
+                "error": "flexibly",
+                "correction": [
+                    "flexible",
+                    "flexibility",
+                    "flexibale"
+                ]
+            }
+        ]
+    }
+    """
+    def azure_spellcheck(essay_text):
+        api_key = "48603fa402a041f9926e5962fbc3fd80"
+
+        endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck"
+        data = {'text': essay_text}
+        params = {
+            'mkt':'en-us',
+            'mode':'proof'
+            }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Ocp-Apim-Subscription-Key': api_key,
+        }
+
+        response = requests.post(endpoint, headers=headers, params=params, data=data).json()
+
+        #Make sure all errors from all APIs get the same format
+
+        response_formatted = []
+        try:
+            for error in response['flaggedTokens']:
+                if error["type"] == "UnknownToken":
+                    error_type = "Mispelled word"
+                else:
+                    error_type = "Unknown error type"
+
+                corrections = []
+                for i in error["suggestions"]:
+                    corrections.append(i["suggestion"])
+
+                error_dict = {
+                    "offset" : error["offset"],
+                    "length" : len(error["token"]),
+                    "message" : error_type,
+                    "type" : error_type,
+                    "error" : error["token"],
+                    "corrections" : corrections
+                }
+
+                response_formatted.append(error_dict)
+
+        except KeyError:
+            print('[ERROR] Azure Spellcheck API call')
+            print(json.dumps(response, indent=4))
+
+        return response_formatted
+
     def grammarbot(essay_text):
         essay_text = essay_text.replace(" ", "%20")
         url = "https://grammarbot.p.rapidapi.com/check"
@@ -36,8 +118,33 @@ def submit(request, essay):
             'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870",
             'content-type': "application/x-www-form-urlencoded"
             }
-        response = requests.request("POST", url, data=payload, headers=headers)
-        return response.text
+        response = requests.request("POST", url, data=payload, headers=headers).json()
+
+        #Make sure all errors from all APIs get the same format
+
+        response_formatted = []
+        try:
+            for error in response['matches']:
+                corrections = []
+                for i in error["replacements"]:
+                    corrections.append(i["value"])
+
+                error_dict = {
+                    "offset" : error["offset"],
+                    "length" : error["length"],
+                    "message" : error["shortMessage"],
+                    "type" : error["rule"]["issueType"],
+                    "error" : essay_text[error["offset"]:error["offset"]+error["length"]],
+                    "corrections" : corrections
+                }
+
+                response_formatted.append(error_dict)
+
+        except KeyError:
+            print('[ERROR] Grammarbot API call')
+            print(json.dumps(response, indent=4))
+
+        return response_formatted
 
     def language_tool(essay_text):
         essay_text = essay_text.replace(" ", "%20")
@@ -48,8 +155,33 @@ def submit(request, essay):
             'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870",
             'content-type': "application/x-www-form-urlencoded"
             }
-        response = requests.request("POST", url, data=payload, headers=headers)
-        return response.text
+        response = requests.request("POST", url, data=payload, headers=headers).json()
+
+        #Make sure all errors from all APIs get the same format
+
+        response_formatted = []
+        try:
+            for error in response['matches']:
+                corrections = []
+                for i in error["replacements"]:
+                    corrections.append(i["value"])
+
+                error_dict = {
+                    "offset" : error["offset"],
+                    "length" : error["length"],
+                    "message" : error["shortMessage"],
+                    "type" : error["rule"]["issueType"],
+                    "error" : essay_text[error["offset"]:error["offset"]+error["length"]],
+                    "corrections" : corrections
+                }
+
+                response_formatted.append(error_dict)
+
+        except KeyError:
+            print('[ERROR] Language Tool API call')
+            print(json.dumps(response, indent=4))
+
+        return response_formatted
 
     def web_spell_checker(essay_text):
         url = "https://webspellchecker-webspellcheckernet.p.rapidapi.com/ssrv.cgi"
@@ -59,55 +191,55 @@ def submit(request, essay):
             'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870"
         }
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        return json.loads(response.text)[0]
+        response = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)[0]
 
-    def aggregate_errors(responses):
-        all_errors = []
-        for response in responses:
-            try:
-                all_errors = all_errors + json.loads(response)['matches']
-            except TypeError:
-                all_errors = all_errors + response['matches']
-        return all_errors
+        #Make sure all errors from all APIs get the same format
 
-    responses = []
-    apis = [grammarbot, language_tool, web_spell_checker]
-    for api in apis:
-        responses.append(api(essay.essay_text))
-    all_errors = aggregate_errors(responses)
+        response_formatted = []
+        try:
+            for error in response['matches']:
+                corrections = []
+                for correction in error["suggestions"]:
+                    corrections.append(correction)
+
+                error_dict = {
+                    "offset" : error["offset"],
+                    "length" : error["length"],
+                    "message" : error["message"],
+                    "type" : error["rule"]["category"],
+                    "error" : essay_text[error["offset"]:error["offset"]+error["length"]],
+                    "corrections" : corrections
+                }
+
+            response_formatted.append(error_dict)
+
+        except KeyError:
+            print('[ERROR] Web Spell Checker API call')
+            print(json.dumps(response, indent=4))
+
+        return response_formatted
+
+    all_errors = []
+    for api in [azure_spellcheck, grammarbot, language_tool, web_spell_checker]:
+        all_errors = all_errors + api(essay.essay_text)
 
     essay.errors = {"content": []}
     #make sure the same offset doesn't repeat
-    offsets = []
-    essay.essay_correction_json = responses
 
     # String adding function
+    offsets = []
     for error in all_errors:
         if error['offset'] not in offsets:
             offsets.append(error['offset'])
             # To offer support for multiple correction suggestions we need to store them as a dictionary
-            correction_dict = {"content": []}
-            #try different names of variables from different APIs
-            try:
-                corrections = error['replacements']
-            except KeyError:
-                corrections = error['suggestions']
-            try:
-                type = error['shortMessage']
-            except KeyError:
-                type = error['rule']['id']
-
-            for correction in corrections:
-                correction_dict["content"].append(correction)
 
             essay.errors["content"].append({
                 "offset": error['offset'],
                 "length": error['length'],
                 "message": error['message'],
-                "type": type,
-                "error": essay.essay_text[error['offset']:error['offset']+error['length']],
-                "correction": json.dumps(correction_dict)
+                "type": error['type'],
+                "error": error['error'],
+                "corrections": error['corrections']
             })
         else:
             pass
@@ -123,15 +255,16 @@ def correction(request):
     def add_strings(original, errors):
         parts = []
         start = 0
+
+        errors = sorted(errors, key = lambda i: i['offset'])
+
         for error in errors:
-            corrections = json.loads(error['correction'])['content']
+            print(error['offset'])
+            corrections = error['corrections']
             correction_text = ""
             for i in range(len(corrections)):
-                #If there is only 1 correction, surround it with quotation marks
-                try:
-                    placeholder = corrections[i]['value']
-                except TypeError:
-                    placeholder = corrections[i]
+                #If there is only 1 correction, don't surround it with parenthesis
+                placeholder = corrections[i]
                 if len(corrections) == 1:
                     correction_text = placeholder
                 else:
--- teacher/test.py
+++ teacher/test.py
@@ -1,4 +1,29 @@
 """
+import requests
+import json
+
+api_key = "48603fa402a041f9926e5962fbc3fd80"
+example_text = "My name is liyan and im a 15 year old feamle and I live in Swedan, but I was born in Somalia. I escaped from somalia becous it was to dangerous, was just a little girl around two years old I think. I came to swedan with a plan. When we landed we met my cusin. They were already here. We waited until we got premission to live here, we got it a week after we came, and now I lived here since then. The first thing you need to know about me is that im muslim girl. I carry a hijab on me evreytime im outside and I'm not forced to it´s my choice, but some people around the world don´t get it. I have a big family we`re 9 people 7 kids, im the middel one I have 3 biger and 3 younger siblings. The house I live in is not that big but big enough to live in, I share room with two of my siblings one is younger and the other one is older, we always argument about who´s gonna clean and making the bed every singel morning. To have a big family is sometimes great, to talk to someone or hang out and other things but the bad to have a bige family is that you never get anything for yourself, sometimes they just take my things without my permission and it´s annoying, I have to clean after my younger siblings all the time. When my relatives come without to telling us" # the text to be spell-checked
+endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck"
+
+data = {'text': example_text}
+
+params = {
+    'mkt':'en-us',
+    'mode':'proof'
+    }
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Ocp-Apim-Subscription-Key': api_key,
+}
+
+response = requests.post(endpoint, headers=headers, params=params, data=data)
+
+json_response = response.json()
+print(json.dumps(json_response, indent=4))
+
+
 
 import requests
 
@@ -31,8 +56,6 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
 
-"""
-
 import requests, xmltodict
 
 import requests
@@ -48,3 +71,48 @@ headers = {
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
+
+
+import requests
+
+url = "https://grammarbot.p.rapidapi.com/check"
+payload = ("language=en-US&text=My name is liyan and im a 15 year old feamle and I live in Swedan, but I was born in Somalia. I escaped from somalia becous it was to dangerous, was just a little girl around two years old I think. I came to swedan with a plan. When we landed we met my cusin. They were already here. We waited until we got premission to live here, we got it a week after we came, and now I lived here since then. The first thing you need to know about me is that im muslim girl. I carry a hijab on me evreytime im outside and I'm not forced to it´s my choice, but some people around the world don´t get it. I have a big family we`re 9 people 7 kids, im the middel one I have 3 biger and 3 younger siblings. The house I live in is not that big but big enough to live in, I share room with two of my siblings one is younger and the other one is older, we always argument about who´s gonna clean and making the bed every singel morning. To have a big family is sometimes great, to talk to someone or hang out and other things but the bad to have a bige family is that you never get anything for yourself, sometimes they just take my things without my permission and it´s annoying, I have to clean after my younger siblings all the time. When my relatives come without to telling us").encode("utf-8")
+headers = {
+    'x-rapidapi-host': "grammarbot.p.rapidapi.com",
+    'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870",
+    'content-type': "application/x-www-form-urlencoded"
+    }
+response = requests.request("POST", url, data=payload, headers=headers)
+print(json.dumps(response, indent=4))
+
+
+import requests, json
+
+url = "https://webspellchecker-webspellcheckernet.p.rapidapi.com/ssrv.cgi"
+querystring = {"format":"json","cmd":"grammar_check","text":"My name is liyan and im a 15 year old feamle and I live in Swedan, but I was born in Somalia. I escaped from somalia becous it was to dangerous, was just a little girl around two years old I think. I came to swedan with a plan. When we landed we met my cusin. They were already here. We waited until we got premission to live here, we got it a week after we came, and now I lived here since then. The first thing you need to know about me is that im muslim girl. I carry a hijab on me evreytime im outside and I'm not forced to it´s my choice, but some people around the world don´t get it. I have a big family we`re 9 people 7 kids, im the middel one I have 3 biger and 3 younger siblings. The house I live in is not that big but big enough to live in, I share room with two of my siblings one is younger and the other one is older, we always argument about who´s gonna clean and making the bed every singel morning. To have a big family is sometimes great, to talk to someone or hang out and other things but the bad to have a bige family is that".encode('utf-8'),"slang":"en_US"}
+headers = {
+    'x-rapidapi-host': "webspellchecker-webspellcheckernet.p.rapidapi.com",
+    'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring).json()
+print(json.dumps(response, indent=4))
+
+"""
+
+import requests, json
+
+essay_text = "My name is liyan and im a 15 year old feamle and I live in Swedan, but I was born in Somalia. I escaped from somalia becous it was to dangerous, was just a little girl around two years old I think. I came to swedan with a plan. When we landed we met my cusin. They were already here. We waited until we got premission to live here, we got it a week after we came, and now I lived here since then. The first thing you need to know about me is that im muslim girl. I carry a hijab on me evreytime im outside and I'm not forced to it´s my choice, but some people around the world don´t get it. I have a big family we`re 9 people 7 kids, im the middel one I have 3 biger and 3 younger siblings. The house I live in is not that big but big enough to live in, I share room with two of my siblings one is younger and the other one is older, we always argument about who´s gonna clean and making the bed every singel morning. To have a big family is sometimes great, to talk to someone or hang out and other things but the bad to have a bige family is that"
+
+url = "https://webspellchecker-webspellcheckernet.p.rapidapi.com/ssrv.cgi"
+
+querystring = {"format":"json","cmd":"grammar_check","text":essay_text,"slang":"en_US"}
+
+headers = {
+    'x-rapidapi-host': "webspellchecker-webspellcheckernet.p.rapidapi.com",
+    'x-rapidapi-key': "7968ecd538msh6231460df8f412ap1f8fe2jsn50cd990fd870"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(json.dumps(json.loads(requests.request("GET", url, headers=headers, params=querystring).text)[0], indent=4))
